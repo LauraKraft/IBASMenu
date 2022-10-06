ccfde2a4f3cd0956f40dd884e97e65197f2c9f85
LauraKraft <laura.kraft.mathiasen@gmail.com>
2022-10-06 11:28:19 +0200

    Kontakt

---
+++ Pages/Kontakt.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBAS_menu.Pages;
+
+public class Kontakt : PageModel
+{
+    private readonly ILogger<Kontakt> _logger;
+
+    public Kontakt(ILogger<Kontakt> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
